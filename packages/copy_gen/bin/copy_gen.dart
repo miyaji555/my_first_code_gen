e0b0177ce35eb009e261275ddb79d092f3ad8415
miyaji555 <kou.sepak@gmail.com>
2023-11-11 21:48:27 +0900

    create copy gen package

---
+++ packages/copy_gen/bin/copy_gen.dart
@@ -0,0 +1,5 @@
+import 'package:copy_gen/copy_gen.dart' as copy_gen;
+
+void main(List<String> arguments) {
+  print('Hello world: ${copy_gen.calculate()}!');
+}
---
+++ packages/copy_gen/test/copy_gen_test.dart
@@ -0,0 +1,8 @@
+import 'package:copy_gen/copy_gen.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
